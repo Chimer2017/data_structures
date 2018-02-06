e544d6f4eae760f38e34bf29088ffa0bd079f105
Andrew Gorovoy <andrewgroovy@gmail.com>
2018-02-06 13:39:25 -0700

    started A3

--- A3/main.cpp
+++ A3/main.cpp
@@ -3,6 +3,21 @@
 #include <sstream>
 using namespace std;
 
+struct city{
+  string cityName;
+  string message;
+  city *next;
+  city() {};
+  city(string initName, city *initNext, string initMessage)
+  {
+    cityName = initName;
+    next = initNext;
+    message = initMessage;
+
+  }
+
+};
+
 int printMenu() {
   string input;
 
@@ -19,15 +34,90 @@ int printMenu() {
   cout << "You selected " << input << endl;
   return stoi(input);
 }
+//*************************BUILD NETWORK OPTION 1************************//
+city *buildNetwork() {
+  string cities[] = {"Los Angeles","Phoenix", "Denver", "Dallas", "St.Louis"
+  , "Chicago" , "Atlanta", "Washington, D.C.", "New York", "Boston"};
+  city *head, *current, *tmp;
+  head = new city(cities[0],nullptr,"");
+  current = new city();
+  current = head;
+  int i = 1;
+  while (i < 10)
+  {
+    tmp = new city(cities[i],nullptr,"");
+    current->next = tmp;
+    current = tmp;
+    i++;
+  }
+  cout << "===Current Path===" << endl;
+  current = head;
+  while(current != nullptr)
+  {
+    cout << current->cityName << " -> ";
+    current = current->next;
+  }
+  cout << "NULL" << endl;
+  cout << "==================" << endl;
+  return head;
+}
+//*************************PRINT NETWORK OPTION 2**********************//
+void printPath(city *head) {
+  city *current;
+  current = new city();
+  cout << "===Current Path===" << endl;
+  current = head;
+  while(current != nullptr)
+  {
+    cout << current->cityName << " -> ";
+    current = current->next;
+  }
+  cout << "NULL" << endl;
+  cout << "==================" << endl;
+}
+//*************************ADD CITY OPTION 3**********************//
+city *addCity(city *head, city *previous, string cityName) {
+  //three use cases
+
+  // add a new networkHead
+
+  // add a new tail
+
+  // insert a city
+
+  return head;
+}
+
 
 
 
 int main() {
-  int selection = printMenu();
+  int selection = 0;
+  city *networkHead;
+  string newCity;
+  string previousCity;
+
   while(selection != 5)
   {
       selection = printMenu();
-      
+      if (selection == 1)
+      {
+        networkHead = buildNetwork();
+        cout << endl;
+      }
+      else if (selection == 2)
+      {
+        printPath(networkHead);
+        cout << endl;
+      }
+      else if (selection == 4)
+      {
+        cout << "Please input the name of new city you would like to add to the network. Press enter when done." << endl;
+        cin >> newCity;
+        cout << "Please input the name of the city that your new city will follow. Press enter when done." << endl;
+        cin >> previousCity;
+        //networkHead = addCity(networkHead,/*SEARCH FUNCTION*/, newCity);
+      }
   }
 
 
--- notes.cpp
+++ notes.cpp
@@ -1,110 +1,85 @@
 #include <iostream>
 using namespace std;
 
-struct amigo {
 
-	string name;
-	int age;
+class Time12 {
+public:
+	Time12();
+	Time12(int h, int m, string xm)
+	{
+		hour = h;
+		min = m;
+		mer = xm;
+
+	}
+private:
+
+	int hour;
+	int min;
+	string mer;
 };
 
-struct listNode {
-	string userName;
-	listNode *link;
-
+struct Node
+{
+	string item;
+	Node *next;
 };
-
 
 int main()
 {
-	amigo jose;
-	amigo *josePtr;
+	Time12 t;
+	string a[] = {"uno", "dos", "tres", "cuatro"};
 
-	josePtr = &jose;
-
-	(*josePtr).name = "Bob";
-	(*josePtr).age = 99;
-	//derefencing struct with pointer
-	//can also use this:
-
-	josePtr->name = "Tom";
-	josePtr->age = 70;
-
-	cout << jose.name;
-	cout << jose.age;
-
-	/*
-	Dynamically allocated memory:
-
-	stack - where memory lives
-	automatic variables get allocated on the stack
-	dynamic variables get allocated at run-time and use the heap
+	// for(int i = 0; i < 4; i++)
+	// {
+	// 	cout << a[i] << endl;
+	// }
 
 
-
-	New Operators:
-
-	new:
-
-	int *pl;
-
-	pl = new int;
-
-	delete: deallocates the memory in the heap for that pointer
-
-	delete pl;
-
-	pl = new int;
-
-*/
+	Node *head, *current, *tmp;
+	head = new Node;
+	current = new Node;
 
 
-  // dynamic array with user input length
+	head->item = a[0];
+	head->next = nullptr;
+	// if( head->next == nullptr )
+	// 	cout << "head->next = nullptr" << endl;
 
-	int n = 3;
-	int *a;
+	current = head;
 
-	// allocate memory for a dynamic array that is n long
-	a = new int[n];
+	int i = 1;
+	while( i < 4 )
+	{
+		// create new node; use tmp to point to it
+		tmp = new Node;
 
-	cout << "Enter 3 ints " << endl;
+		// populate the payload of tmp with i-th array element
+		tmp->item = a[i];
+		// in case it's the last element, set current node to indicate end
+		tmp->next = nullptr;
 
-	// fill the array with user input
-	for( int i = 0; i < n ; i++ )
-		cin >> a[i];
+		// at this point "current" is still pointing to previous element
+		// (could be head)
+		// connect the previous element such that it is pointing to the newly created one (tmp)
+		current->next = tmp;
 
-	int *temp;
+		// move forward. Make current point to the new element
+		current = tmp;
 
-	// allocate memory for a dynamic array that is 2n long
-	temp = new int[2*n];
+		i++;
 
-	// copy the contents of array pointed to by a into first
-	// n elements of array pointed to by temp
-	for( int i = 0; i < n ; i++ )
-		temp[i] = a[i];
+	}
 
-	delete [] a; // return memory to heap
+	cout << "Traverse and print the LL: " << endl;
+	current = head;
 
-	a = temp; // assign a to point to temp
-
-	// delete [] temp; // DON'T DO THIS here. We don't actually want to free that memory space back to heap.
-	temp = nullptr;    // To be safe, we can set temp to null so we don't accidentally do something with it later.
-
-	// We can now ask the user to enter 3 more values into our dynamically allocated array
-	cout << "Enter 3 more ints: " << endl;
-
-	for( int i = n; i < 2*n; i++ )
-		cin >> a[i];
-
-	// Print the contents of the whole thing
-	cout << "\n\n";
-	for( int i = 0; i < 2*n; i++ )
-		cout << a[i] << endl;
-
-	// Now that we are finished using a, release the memory it's pointing to back into the heap
-	delete [] a;
-
-	// To be safe, don't point to a memory space that has now been de-allocated
-	a = nullptr;
+	while( current != nullptr )
+	{
+		cout << current->item << endl;
+		cout << current << endl;
+		current = current->next;
+	}
 
 
 	return 0;
