ca34a12611fc735092dc8f91c39fe78e5c1e77d1
Andrew Gorovoy <andrewgroovy@gmail.com>
2018-04-21 17:22:01 -0600

    started working on heap PQ

---
+++ F/PQH.cpp
@@ -0,0 +1,23 @@
+#include "PQH.hpp"
+using namespace std;
+
+
+mh::mh(int cap)
+{
+
+}
+
+void mh::minHeapify(node*)
+{
+
+}
+
+node mh::dequeueH()
+{
+
+}
+
+void mh::enqueueH()
+{
+  
+}
---
+++ F/PQH.hpp
@@ -0,0 +1,38 @@
+#ifndef MH_H
+#define MH_H
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+
+
+struct node {
+  string name;
+  int pri;
+  int treat;
+  node(){};
+  node(n,p,t)
+  {
+    name = n;
+    pri = p;
+    treat = t;
+  }
+};
+
+
+class mh {
+  node *harr;
+  int arraySize;
+  int heapSize;
+public:
+  mh(){};
+  mh(int);
+  void minHeapify(node*);
+  int left(int i) {return (2*i + 1);}
+  int right(int i) {return (2*i + 2);}
+  int parent(int i) {return ((i-1)/2);}
+  node dequeueH();
+  void enqueueH();
+};
+#endif
